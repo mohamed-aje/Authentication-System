94703ca570144aa93b2658166069104564c9a2b4
mohamed-aje <chochoaj93@gmail.com>
2023-02-09 15:59:07 +0200

    established a backend and routes and installed postgres and connected to server

---
+++ server/database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE recipes(
+    rec_id SERIAL PRIMARY KEY,
+    name   VARCHAR(255),
+    describtion TEXT
+)
---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const { response, request } = require("express");
+require("dotenv").config();
+
+//middlware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//ROUTES
+app.post("/recipes", async (request, response) => {
+  try {
+    const { name, describtion } = request.body;
+    const newRecipe = await pool.query(
+      "INSERT INTO recipes (name,describtion) VALUES($1,$2) RETURNING *",
+      [name, describtion]
+    );
+    response.json(newRecipe);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.get("/recipes", async (request, response) => {
+  try {
+    const allRecipes = await pool.query("SELECT * FROM recipes");
+    response.json(allRecipes);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.get("recipes/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const recipe = await pool.query("SELECT * FROM recipe WHERE rec_id = $1", [
+      id,
+    ]);
+    response.json(recipe.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
