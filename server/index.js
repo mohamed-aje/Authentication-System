dcdf0bf6e6cf5146e1b3b51f2d16fe989e086bfa
mohamed-aje <chochoaj93@gmail.com>
2023-02-12 00:38:40 +0200

    created new routes for authentication and autherization

--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ const app = express();
 var cors = require("cors");
 
 require("dotenv").config();
-const router = require("./routes/recipes");
-require("dotenv").config();
 
 //middlware
 app.use(cors());
 app.use(express.json()); //req.body
 
 //ROUTES
+app.use("/auth", require("./routes/auth"));
 app.use("/", require("./routes/recipes"));
 
 const PORT = 3001;
--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,2 +1,61 @@
-const router = require("express").Router();
+const { request, response } = require("express");
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
+
+router.post("/register", async (request, response) => {
+  const { email, name, password } = request.body;
+
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length > 0) {
+      return response.status(401).json("User already exist!");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+    let newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
+
+    const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+    console.log(jwtToken);
+    return response.json({ jwtToken });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500).send("Server error");
+  }
+});
+//login route
+router.post("/login", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    const user = await pool.query(
+      "SELECT * FROM users WHERE user_email = $1 ",
+      [email]
+    );
+    //check if user exist
+    if (user.rows.length === 0) {
+      return response.status(401).json("Password or Email is incorrect.");
+    }
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+    if (!validPassword) {
+      return response.status(401).json("Paswword or Email is incorrect.");
+    }
+    const token = jwtGenerator(user.rows[0].user_id);
+    response.json({ token });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 module.exports = router;
